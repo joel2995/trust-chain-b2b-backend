a50ce14e6bb26dfe7f4ab53fc2df2f50edc3f45e
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-03 03:41:20 +0530

    transaction sync with razorpay and  updated

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {
-  createTransaction,
+  //createTransaction,
   updateTransactionStatus,
 } from "../controllers/transactionController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createTransaction);
+//router.post("/", protect, createTransaction);
 router.put("/:id/status", protect, updateTransactionStatus);
 
 export default router;

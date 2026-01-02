53618eef8957e253d7c05e515757ad1b7084dc53
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-02 23:06:57 +0530

    dashboard updated

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,19 +1,40 @@
 import Transaction from "../models/Transaction.js";
-import User from "../models/User.js";
 
 export const getDashboardSummary = async (req, res) => {
-  const userId = req.user._id;
+  try {
+    const userId = req.user._id;
 
-  const [txCount, completed, activeEscrow] = await Promise.all([
-    Transaction.countDocuments({ $or: [{ buyerId: userId }, { vendorId: userId }] }),
-    Transaction.countDocuments({ status: "completed", $or: [{ buyerId: userId }, { vendorId: userId }] }),
-    Transaction.countDocuments({ "escrow.released": false, $or: [{ buyerId: userId }, { vendorId: userId }] }),
-  ]);
+    const [txCount, completed, escrowAgg] = await Promise.all([
+      Transaction.countDocuments({
+        $or: [{ buyerId: userId }, { vendorId: userId }],
+      }),
+      Transaction.countDocuments({
+        status: "completed",
+        $or: [{ buyerId: userId }, { vendorId: userId }],
+      }),
+      Transaction.aggregate([
+        {
+          $match: {
+            "escrow.released": false,
+            $or: [{ buyerId: userId }, { vendorId: userId }],
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            total: { $sum: "$totalAmount" },
+          },
+        },
+      ]),
+    ]);
 
-  res.json({
-    trustScore: req.user.overallTrustScore,
-    totalTransactions: txCount,
-    completedTransactions: completed,
-    activeEscrows: activeEscrow,
-  });
+    res.json({
+      trustScore: req.user.overallTrustScore,
+      totalTransactions: txCount,
+      completedTransactions: completed,
+      escrowAmount: escrowAgg[0]?.total || 0,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };

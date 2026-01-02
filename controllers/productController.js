06a75bcb1187ef0c304cd684c330b5da15ce3067
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-02 23:05:25 +0530

    Product updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,42 +2,91 @@ import Product from "../models/Product.js";
 
 // Vendor creates product
 export const createProduct = async (req, res) => {
-  const product = await Product.create({
-    ...req.body,
-    vendorId: req.user._id,
-  });
-  res.status(201).json(product);
+  try {
+    if (req.user.role !== "vendor") {
+      return res.status(403).json({ msg: "Only vendors can create products" });
+    }
+
+    const product = await Product.create({
+      ...req.body,
+      vendorId: req.user._id,
+    });
+
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Marketplace list
 export const getProducts = async (req, res) => {
-  const products = await Product.find({ active: true })
-    .populate("vendorId", "name overallTrustScore");
-  res.json(products);
+  try {
+    const products = await Product.find({ active: true })
+      .populate("vendorId", "name overallTrustScore");
+
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Single product
 export const getProductById = async (req, res) => {
-  const product = await Product.findById(req.params.id)
-    .populate("vendorId", "name overallTrustScore");
-  res.json(product);
+  try {
+    const product = await Product.findById(req.params.id)
+      .populate("vendorId", "name overallTrustScore");
+
+    if (!product) {
+      return res.status(404).json({ msg: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Vendor update
 export const updateProduct = async (req, res) => {
-  const product = await Product.findOneAndUpdate(
-    { _id: req.params.id, vendorId: req.user._id },
-    req.body,
-    { new: true }
-  );
-  res.json(product);
+  try {
+    if (req.user.role !== "vendor") {
+      return res.status(403).json({ msg: "Only vendors can update products" });
+    }
+
+    const product = await Product.findOneAndUpdate(
+      { _id: req.params.id, vendorId: req.user._id },
+      req.body,
+      { new: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({ msg: "Product not found or unauthorized" });
+    }
+
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Vendor delete
 export const deleteProduct = async (req, res) => {
-  await Product.findOneAndDelete({
-    _id: req.params.id,
-    vendorId: req.user._id,
-  });
-  res.json({ msg: "Product deleted" });
+  try {
+    if (req.user.role !== "vendor") {
+      return res.status(403).json({ msg: "Only vendors can delete products" });
+    }
+
+    const deleted = await Product.findOneAndDelete({
+      _id: req.params.id,
+      vendorId: req.user._id,
+    });
+
+    if (!deleted) {
+      return res.status(404).json({ msg: "Product not found or unauthorized" });
+    }
+
+    res.json({ msg: "Product deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     name: { type: String, required: true },
     description: String,
     price: { type: Number, required: true },

e6e8d34f9d8272427a6c0f61abf5b15d5138145d
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-03 20:10:04 +0530

    block route updation

--- routes/blockchainRoutes.js
+++ routes/blockchainRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { writeProof } from "../controllers/blockchainController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { idempotency } from "../middleware/idempotency.js";
 
 const router = express.Router();
 
-router.post("/write", protect, writeProof);
+router.post("/write", protect, idempotency, writeProof);
 
 export default router;

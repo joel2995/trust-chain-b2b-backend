89bc85208dda5948c2f8882b14eb8e85938b6570
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-03 22:10:49 +0530

    add request validation to /write route in blockchainRoutes

--- routes/blockchainRoutes.js
+++ routes/blockchainRoutes.js
@@ -2,9 +2,17 @@ import express from "express";
 import { writeProof } from "../controllers/blockchainController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { idempotency } from "../middleware/idempotency.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { writeProofSchema } from "../validators/blockchain.validator.js";
 
 const router = express.Router();
 
-router.post("/write", protect, idempotency, writeProof);
+router.post(
+  "/write",
+  protect,
+  idempotency,
+  validateRequest(writeProofSchema),
+  writeProof
+);
 
 export default router;

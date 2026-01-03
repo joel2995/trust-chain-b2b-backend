7d18d83669cfa935bbd6e199e8215249cf437c15
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-03 22:10:11 +0530

    add validateRequest middleware for request validation

---
+++ middleware/validateRequest.js
@@ -0,0 +1,17 @@
+export const validateRequest = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+      allowUnknown: false,
+    });
+
+    if (error) {
+      return res.status(400).json({
+        error: "Validation error",
+        details: error.details.map(d => d.message),
+      });
+    }
+
+    next();
+  };
+};

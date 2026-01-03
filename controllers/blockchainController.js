fea86f7d6eb2ef59194aec107b6fd27e5bcf0442
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-03 18:47:37 +0530

    controller block updated

--- controllers/blockchainController.js
+++ controllers/blockchainController.js
@@ -3,11 +3,17 @@ import { storeProofOnChain } from "../services/blockchainService.js";
 export const writeProof = async (req, res) => {
   try {
     const { cid, fileHash } = req.body;
-    if (!cid || !fileHash)
-      return res.status(400).json({ msg: "Missing data" });
+
+    if (!cid || !fileHash) {
+      return res.status(400).json({ msg: "Missing cid or fileHash" });
+    }
 
     const proof = await storeProofOnChain({ cid, fileHash });
-    res.json({ msg: "Stored on blockchain", proof });
+
+    res.status(200).json({
+      msg: "Stored on blockchain",
+      proof,
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }

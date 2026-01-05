d44560dd2cbb0ef0debb6128b8d2a657d643666e
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-05 17:15:40 +0530

    transaction model updation

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,40 +1,44 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  buyerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  vendorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  amount: { type: Number, required: true },
-productId: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: "Product",
-  required: true,
-},
-quantity: { type: Number, required: true },
+const transactionSchema = new mongoose.Schema(
+  {
+    buyerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: { type: Number, required: true },
+    amount: { type: Number, required: true },
 
-  razorpayOrderId: String,
-  razorpayPaymentId: String,
+    status: {
+      type: String,
+      enum: [
+        "CREATED",
+        "PAID",
+        "IN_ESCROW",
+        "DELIVERED",
+        "RELEASED",
+        "DISPUTED",
+        "REFUNDED",
+      ],
+      default: "CREATED",
+    },
 
-  status: {
-    type: String,
-    enum: [
-      "CREATED",
-      "PAID",
-      "IN_ESCROW",
-      "DELIVERED",
-      "RELEASED",
-      "DISPUTED",
-      "REFUNDED",
-    ],
-    default: "CREATED",
+    // 🔐 Delivery proof
+    deliveryProofHash: String,
+    deliveryProofTxHash: String,
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export default mongoose.model("Transaction", transactionSchema);

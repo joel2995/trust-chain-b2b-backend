9be8fac004648eeba653be105d274c8fe46727ad
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-07 02:40:20 +0530

    admin protect middleware

--- routes/adminAuthRoutes.js
+++ routes/adminAuthRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { adminLogin } from "../controllers/adminAuthController.js";
+import { adminLogin, getCurrentAdmin } from "../controllers/adminAuthController.js"
+import { adminProtect } from "../middleware/adminAuthMiddleware.js"
 
 const router = express.Router();
 
 router.post("/login", adminLogin);
+router.get("/me", adminProtect, getCurrentAdmin)
 
 export default router;
